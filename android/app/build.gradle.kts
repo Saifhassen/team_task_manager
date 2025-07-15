5f609e00da63cc64b705e0ccdea8a98ae0985f67
Saif Hassan <your-email@alitiger1122334455gmali.com>
2025-07-15 23:34:26 +0300

    fix: upgrade flutter_local_notifications and set NDK version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,8 @@ plugins {
 android {
     namespace = "com.example.team_task_manager"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
