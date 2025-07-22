41bc2bc1ecbfd81d9ba71fe9a6ac7e0538aef59f
Saif Hassan <your-email@alitiger1122334455gmali.com>
2025-07-22 22:37:25 +0300

    Initial commit

--- ios/podhelper.rb
+++ ios/podhelper.rb
@@ -1,35 +1,53 @@
 require 'fileutils'
+require 'json'
 
 def flutter_root
   generated_xcode_build_settings_path = File.expand_path(File.join('..', 'Flutter', 'Generated.xcconfig'), __dir__)
   unless File.exist?(generated_xcode_build_settings_path)
-    raise "#{generated_xcode_build_settings_path} must exist. If you're running pod install manually, make sure flutter pub get is executed first"
+    raise "#{generated_xcode_build_settings_path} must exist. Run 'flutter pub get' first."
   end
 
   File.foreach(generated_xcode_build_settings_path) do |line|
-    matches = line.match(/FLUTTER_ROOT\=(.*)/)
+    matches = line.match(/FLUTTER_ROOT=(.*)/)
     return matches[1].strip if matches
   end
-  raise "FLUTTER_ROOT not found in #{generated_xcode_build_settings_path}. Try running flutter pub get first."
+
+  raise "FLUTTER_ROOT not found in #{generated_xcode_build_settings_path}. Run 'flutter pub get' first."
 end
 
 def flutter_ios_engine_podspec
-  File.expand_path(File.join(flutter_root, 'bin', 'cache', 'artifacts', 'engine', 'ios_engine.podspec'))
-end
-
-def flutter_install_all_ios_pods(flutter_application_path)
-  install_all_flutter_pods(flutter_application_path)
+  File.expand_path(File.join(flutter_root, 'bin', 'cache', 'artifacts', 'engine', 'ios', 'Flutter.podspec'))
 end
 
 def install_all_flutter_pods(flutter_application_path)
-  flutter_install_ios_engine_pod
-  flutter_install_podspecs(flutter_application_path)
+  install_flutter_engine_pod
+  install_flutter_plugin_pods(flutter_application_path)
 end
 
-def flutter_install_ios_engine_pod
+def install_flutter_engine_pod
   pod 'Flutter', :podspec => flutter_ios_engine_podspec
 end
 
-def flutter_install_podspecs(flutter_application_path)
-  Dir.glob(File.join(flutter_application_path, '.ios', 'Flutter', 'podspecs', '*.podspec')).each do |podspec|
-    podname = File
+def install_flutter_plugin_pods(flutter_application_path)
+  symlinks_dir = File.expand_path('.symlinks', flutter_application_path)
+  plugins_file = File.join(flutter_application_path, '.flutter-plugins-dependencies')
+
+  return unless File.exist?(plugins_file)
+
+  plugins = JSON.parse(File.read(plugins_file))['plugins']
+  plugins['ios'].each do |plugin|
+    name = plugin['name']
+    path = plugin['path']
+    symlink = File.join(symlinks_dir, name)
+    FileUtils.mkdir_p(symlinks_dir)
+    FileUtils.rm_f(symlink)
+    FileUtils.ln_s(path, symlink)
+    pod name, :path => File.join(symlink, 'ios')
+  end
+end
+
+def flutter_additional_ios_build_settings(target)
+  target.build_configurations.each do |config|
+    config.build_settings['EXCLUDED_ARCHS[sdk=iphonesimulator*]'] = 'arm64'
+  end
+end
