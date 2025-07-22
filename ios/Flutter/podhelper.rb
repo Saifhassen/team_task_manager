551968a8b7ed18245128cb720e351dcfa8f55483
Saif Hassan <your-email@alitiger1122334455gmali.com>
2025-07-22 21:08:53 +0300

    تحديث podfile ومجلد iOS

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -1,26 +1,35 @@
-platform :ios, '12.0'
+require 'fileutils'
 
-ENV['COCOAPODS_DISABLE_STATS'] = 'true'
-
-project 'Runner', {
-  'Debug' => :debug,
-  'Profile' => :release,
-  'Release' => :release,
-}
-
-# تحميل سكربت flutter الخاص بالـ CocoaPods
-flutter_application_path = File.expand_path('..', __dir__)
-load File.join(flutter_application_path, 'ios', 'Flutter', 'podhelper.rb')
-
-target 'Runner' do
-  use_frameworks!
-  use_modular_headers!
-
-  flutter_install_all_ios_pods(flutter_application_path)
-end
-
-post_install do |installer|
-  installer.pods_project.targets.each do |target|
-    flutter_additional_ios_build_settings(target)
+def flutter_root
+  generated_xcode_build_settings_path = File.expand_path(File.join('..', 'Flutter', 'Generated.xcconfig'), __dir__)
+  unless File.exist?(generated_xcode_build_settings_path)
+    raise "#{generated_xcode_build_settings_path} must exist. If you're running pod install manually, make sure flutter pub get is executed first"
   end
+
+  File.foreach(generated_xcode_build_settings_path) do |line|
+    matches = line.match(/FLUTTER_ROOT\=(.*)/)
+    return matches[1].strip if matches
+  end
+  raise "FLUTTER_ROOT not found in #{generated_xcode_build_settings_path}. Try running flutter pub get first."
 end
+
+def flutter_ios_engine_podspec
+  File.expand_path(File.join(flutter_root, 'bin', 'cache', 'artifacts', 'engine', 'ios_engine.podspec'))
+end
+
+def flutter_install_all_ios_pods(flutter_application_path)
+  install_all_flutter_pods(flutter_application_path)
+end
+
+def install_all_flutter_pods(flutter_application_path)
+  flutter_install_ios_engine_pod
+  flutter_install_podspecs(flutter_application_path)
+end
+
+def flutter_install_ios_engine_pod
+  pod 'Flutter', :podspec => flutter_ios_engine_podspec
+end
+
+def flutter_install_podspecs(flutter_application_path)
+  Dir.glob(File.join(flutter_application_path, '.ios', 'Flutter', 'podspecs', '*.podspec')).each do |podspec|
+    podname = File
--- ios/podhelper.rb
+++ ios/podhelper.rb
@@ -1,82 +1,35 @@
 require 'fileutils'
-require 'json'
-
-def parse_KV_file(file, separator = '=')
-  return {} unless File.exist?(file)
-
-  result = {}
-  skip_line_start_symbols = ['#', '/']
-
-  File.foreach(file) do |line|
-    next if skip_line_start_symbols.any? { |symbol| line =~ /^\s*#{symbol}/ }
-
-    key_value = line.strip.split(separator, 2)
-    next unless key_value.length == 2
-
-    key = key_value[0].strip
-    value = key_value[1].strip.gsub(/\A['"]|['"]\Z/, '')
-    result[key] = value
-  end
-
-  result
-end
 
 def flutter_root
-  config = parse_KV_file(File.expand_path('Generated.xcconfig', __dir__))
-  unless config['FLUTTER_ROOT']
-    abort('❌ FLUTTER_ROOT not found. Did you run `flutter pub get`?')
+  generated_xcode_build_settings_path = File.expand_path(File.join('..', 'Flutter', 'Generated.xcconfig'), __dir__)
+  unless File.exist?(generated_xcode_build_settings_path)
+    raise "#{generated_xcode_build_settings_path} must exist. If you're running pod install manually, make sure flutter pub get is executed first"
   end
-  config['FLUTTER_ROOT']
+
+  File.foreach(generated_xcode_build_settings_path) do |line|
+    matches = line.match(/FLUTTER_ROOT\=(.*)/)
+    return matches[1].strip if matches
+  end
+  raise "FLUTTER_ROOT not found in #{generated_xcode_build_settings_path}. Try running flutter pub get first."
 end
 
-def flutter_ios_engine_path
-  File.join(flutter_root, 'bin', 'cache', 'artifacts', 'engine', 'ios')
-end
-
-def flutter_ios_podspec_paths
-  Dir.glob(File.join(flutter_ios_engine_path, '**/*.podspec'))
+def flutter_ios_engine_podspec
+  File.expand_path(File.join(flutter_root, 'bin', 'cache', 'artifacts', 'engine', 'ios_engine.podspec'))
 end
 
 def flutter_install_all_ios_pods(flutter_application_path)
-  packages_path = File.join(flutter_application_path, '.packages')
-  unless File.exist?(packages_path)
-    abort("❌ '#{flutter_application_path}' must be the root of your Flutter project. Run `flutter pub get` first.")
-  end
-
-  podspecs = flutter_ios_podspec_paths
-  if podspecs.empty?
-    abort("❌ No podspecs found in #{flutter_ios_engine_path}. Run `flutter precache`.")
-  end
-
-  podspecs.each do |podspec_path|
-    podname = File.basename(podspec_path, '.podspec')
-    pod podname, :path => podspec_path
-  end
-
-  # إعداد روابط الإضافات (plugins)
-  plugins_file = File.join(flutter_application_path, '.flutter-plugins-dependencies')
-  unless File.exist?(plugins_file)
-    abort("❌ Missing .flutter-plugins-dependencies. Run `flutter pub get`.")
-  end
-
-  plugins = JSON.parse(File.read(plugins_file))['plugins']['ios']
-  symlinks_dir = File.join(flutter_application_path, '.symlinks')
-  FileUtils.mkdir_p(symlinks_dir)
-
-  plugins.each do |plugin|
-    plugin_name = plugin['name']
-    plugin_path = plugin['path']
-    symlink = File.join(symlinks_dir, plugin_name)
-    FileUtils.rm_f(symlink)
-    File.symlink(plugin_path, symlink)
-    pod plugin_name, :path => symlink
-  end
+  install_all_flutter_pods(flutter_application_path)
 end
 
-def flutter_additional_ios_build_settings(target)
-  target.build_configurations.each do |config|
-    config.build_settings['ENABLE_BITCODE'] = 'NO'
-    config.build_settings['EXCLUDED_ARCHS[sdk=iphonesimulator*]'] = 'arm64'
-    config.build_settings['BUILD_LIBRARY_FOR_DISTRIBUTION'] = 'YES'
-  end
+def install_all_flutter_pods(flutter_application_path)
+  flutter_install_ios_engine_pod
+  flutter_install_podspecs(flutter_application_path)
 end
+
+def flutter_install_ios_engine_pod
+  pod 'Flutter', :podspec => flutter_ios_engine_podspec
+end
+
+def flutter_install_podspecs(flutter_application_path)
+  Dir.glob(File.join(flutter_application_path, '.ios', 'Flutter', 'podspecs', '*.podspec')).each do |podspec|
+    podname = File
